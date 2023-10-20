a7bbcd27f668360228cdb842fb60b998e38fded8
Bharat87Bist <94631737+Bharat87Bist@users.noreply.github.com>
2023-10-20 01:24:14 +0000

    datatypes over

---
+++ Basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict" //treate all JS code as a newer version
+// alert("hello") we are using node js not browser it will only supported by browser only 
+let isloggedin = true
+null
+let state
+//null = standlone value
+// symbols = used for uniqueness or when we are working on react
+console.log(typeof "Bharat");
+console.log(typeof null);
+console.log(typeof isloggedin);
+console.log(typeof state);
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const id=123123
+let name ="Bharat"
+var email="bharatbist@gmail.com"
+address="garudachorpalya"
+/* now we should not use var keyword to create variable in javascript
+because of issue in block soope and functional scope
+*/
+let username;
+console.table([id,name,email,address,username]);
