a8dc08829ac8a3ef183fd173227e69e91cef8621
Bharat87Bist <94631737+Bharat87Bist@users.noreply.github.com>
2023-10-20 01:56:22 +0000

    conversion done

---
+++ Basics/conversion.js
@@ -0,0 +1,41 @@
+// let score ="kjsdh"
+
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isloggetIn="dfs"
+let booleanisloggedin=Boolean(isloggetIn)
+console.log(booleanisloggedin);
+
+
+let someNumber=33
+let stringNummber=String(someNumber)
+console.log(typeof stringNummber);
+
+
+// *******************. Operations .*********************
+
+// let value=5
+// let negval=-value
+// console.log(negval);
+// console.log(3*3);
+// console.log(3**3);
+// let str1="bharat"
+// let str2="bist"
+// let str3=str1+str2 
+// console.log(str3);
+// console.log("1"+3);
+// console.log("1"+2+2);
+// console.log(2+2+"2");
+
+
+// let num1,num2,num3
+// num1=num2=num3=4+4
+// console.log(num2);
+
+
+ let gamecounter=100
+ ++gamecounter
+ console.log(gamecounter);
