7669344771e556e5228bbc489d752fb09ebae878
Bharat87Bist <94631737+Bharat87Bist@users.noreply.github.com>
2023-10-21 08:40:55 +0000

    maths done

---
+++ Basics/numsAndMaths.js
@@ -0,0 +1,35 @@
+// const score=400
+// const balance=new Number(100)
+
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const another =123.877342
+// console.log(another.toPrecision(4));
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+
+//++++++++++++++++++++++++++ Maths.  +++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-345));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,4,5,6));
+
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
